f05605b874fce0a045a9632d7be25382ac80272a
KevM7 <34063604+KevM7@users.noreply.github.com>
2018-07-01 18:59:28 +0200

    Feat : LCD wifi on 192.168.0.27

---
+++ lcdwifi.ino
@@ -0,0 +1,46 @@
+#include <LiquidCrystal.h>
+const int RS = 12;
+const int E = 11;
+const int D4 = 5;
+const int D5 = 4;
+const int D6 = 3;
+const int D7 = 2;
+
+const int COLS = 16;
+const int ROWS = 2;
+
+int incomingByte = 0;
+
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);  
+  Serial1.begin(9600);
+  lcd.begin(COLS, ROWS);
+  //lcd.setCursor(0, 0);
+  //lcd.autoscroll();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (Serial1.available() > 0) {
+    delay(100);
+    Serial.println("prêt");
+    lcd.clear();
+    //lcd.setCursor(0, 0);
+    int cpt = 0;
+    while (Serial1.available() > 0) {
+      incomingByte = Serial1.read();
+
+      Serial.print("I received : ");
+      Serial.println(incomingByte, DEC);
+      Serial.println(char(incomingByte)); 
+      
+      lcd.print(char(incomingByte));
+      cpt += 1;
+    }
+  }
+}
---
+++ nodeMcuLCD.ino
@@ -0,0 +1,90 @@
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+ 
+const char* ssid = "KM7";
+const char* password = "KEVIO_007";
+ 
+SoftwareSerial uno(16, 5); // RX TX
+WiFiServer server(80);
+ 
+void setup() {
+  uno.begin(9600);
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("Ready");
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+  if (request.indexOf("/?name=") != -1)  {
+    Serial.println("ON");
+    String mess = request.substring(request.indexOf("/?name=") + 7, request.indexOf("HTTP/1.1"));
+    mess.replace("+", " ");
+    Serial.println(mess);
+    uno.print(mess);
+  }
+ 
+// Set ledPin according to the request
+//digitalWrite(ledPin, value);
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+
+  client.println("<form action=\"\" method=\"GET\">");
+  client.println("<input type=\"text\" id=\"uname\" name=\"name\">");
+  client.println("<button type=\"submit\">Envoyer votre message</button>");
+  client.println("</form>");
+  client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+ 
+}
